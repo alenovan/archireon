5c5cc3e1b419ddf36a50e0af1534211b51d5e3a6
alenovan <novannanega@gmail.com>
2018-04-20 15:11:11 +0700

    one

---
+++ LoginActivity.java
@@ -0,0 +1,13 @@
+package com.onetech.novan.archirecon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
